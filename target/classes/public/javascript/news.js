9e78133c6faddd4d3f47579624f33c2e2a3a0d42
myzozoz <markusp_holopainen@yahoo.com>
2017-12-07 18:41:59 +0200

    Script fetches news for front page

---
+++ target/classes/public/javascript/news.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   $("#newslist").append("<ol/>");
+
+   $.getJSON("http://localhost:8080/news", function(data){
+       $.each(data, function(i, article){
+          $("ol").append("<li>" + article.title + "</li>");
+       });
+   });
+
+
+});
