7496e4ff2e7fb46aa1bd9a9aada8b2a95983e32f
myzozoz <markusp_holopainen@yahoo.com>
2017-12-07 12:28:23 +0200

    Added a single fluke test so Travis can be integrated

--- src/main/java/wad/NFTBApplication.java
+++ src/main/java/wad/NFTBApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class nftbApplication {
+public class NFTBApplication {
     public static void main(String[] args) {
-        SpringApplication.run(nftbApplication.class, args);
+        SpringApplication.run(NFTBApplication.class, args);
     }
 }
---
+++ src/test/java/wad/NFTBApplicationTest.java
@@ -0,0 +1,31 @@
+package wad;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NFTBApplicationTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void emptyTest() {
+        Assert.assertNotNull(context);
+    }
+}
