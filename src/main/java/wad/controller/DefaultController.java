e96321a8ae855af5ced67781bf2c10d83faa1174
myzozoz <markusp_holopainen@yahoo.com>
2017-12-07 14:19:13 +0200

    small edits in DefaultController

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("*")
-    public String defaultRedirect(){
+    @GetMapping("/")
+    public String root() {
         return "index";
     }
 }
