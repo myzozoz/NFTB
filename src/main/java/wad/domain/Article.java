110912fdd0d8c52f729ffd7d6d217d869ab2c2c6
myzozoz <markusp_holopainen@yahoo.com>
2017-12-07 15:56:36 +0200

    Added a local scetch file to .gitignore

---
+++ src/main/java/wad/domain/Article.java
@@ -0,0 +1,16 @@
+package wad.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Article {
+
+    
+}
