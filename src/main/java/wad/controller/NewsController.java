953635874780e73290da0df30a4057af6a2036d5
myzozoz <markusp_holopainen@yahoo.com>
2017-12-07 16:30:07 +0200

    Created controller .java files

---
+++ src/main/java/wad/controller/NewsController.java
@@ -0,0 +1,10 @@
+package wad.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewsController {
+
+}
